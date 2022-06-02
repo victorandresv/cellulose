4ffe85bc0c03a61f62a0d32b0414148d4f5907e6
Victor Vargas <victorvargas@MacBook-Pro-de-Victor.local>
2022-06-02 14:27:50 -0300

    read env file

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module cielo
 
 go 1.16
+
+require github.com/joho/godotenv v1.4.0 // indirect
--- pay.go
+++ pay.go
@@ -1,5 +1,7 @@
 package main
 
-func main() {
+import "github.com/joho/godotenv"
 
+func main() {
+	godotenv.Load()
 }
